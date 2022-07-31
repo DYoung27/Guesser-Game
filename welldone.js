38f1728752f35965111f7f960587d58f534160ec
D Young <106049417+DYoung27@users.noreply.github.com>
2022-07-31 14:41:08 +0100

    Delete welldone.js

--- welldone.js
+++
@@ -1,5 +0,0 @@
-let victory = new Audio('victory.mp3')
-victory.play()
-
-let close = document.getElementById('close-btn');
-close.onclick = () => setTimeout(() => {window.open('','_self').close()}, 300);
